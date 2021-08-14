3cda15e340e2c7ca09903871082afb98fda41440
Rupinderj <65835380+Rupinderj@users.noreply.github.com>
2021-08-13 22:43:17 -0400

    Update Program.cs

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -15,17 +15,38 @@ namespace BreadmakerReport
         {
             Console.WriteLine("Welcome to Bread World");
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
-            var BMList = BreadmakerDb.Breadmakers
+            var a = BreadmakerDb.Breadmakers
                 // TODO: add LINQ logic ...
                 //       ...
+                .Select(item => new
+            {
+                detail = item.title,
+                reviews = item.Reviews.Count(),
+                avg = (Double)BreadmakerDb.Reviews
+                    .Where(i =>i.BreadmakerId == item.BreadmakerId)
+                    .Select(i => i.stars).Sum() / item.Reviews.Count(),
+            })
+
+                .ToList();
+
+                var BMList = a
+                .Select(item => new
+                {
+                    detail = item.detail,
+                    reviews = item.reviews,
+                    avg = item.avg,
+                    adjust = ratingAdjustmentService.Adjust(item.avg, item.reviews)
+                })
+                .OrderByDescending(i => i.adjust)
                 .ToList();
 
             Console.WriteLine("[#]  Reviews Average  Adjust    Description");
             for (var j = 0; j < 3; j++)
             {
-                var i = BMList[j];
+                var item = BMList[j];
                 // TODO: add output
                 // Console.WriteLine( ... );
+                Console.WriteLine($"[{j + 1}]  {item.reviews,7}  {Math.Round(item.avg, 2),-7}  {Math.Round(item.adjust, 2),-6}   {item.detail}");
             }
         }
     }
